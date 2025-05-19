7af5991fa2c43cd3708004af6b35d7fb40ef4655
Lola Victoria <damilolaoniyide11@gmail.com>
2025-05-19 23:46:03 +0100

    fixed ui

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -146,7 +146,7 @@ const Auth = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
 
       <div className="flex items-center space-x-2">
         <div className="h-12 w-12 rounded-full bg-cybersafe-600 flex items-center justify-center">
